050451d1ec32545915439d65cba080c468535620
jaf1x <zepy27012010@gmail.com>
2025-04-27 17:30:40 +0300

    dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
+        // scaffoldBackgroundColor: const Color.fromARGB(221, 30, 30, 30),
+        scaffoldBackgroundColor: const Color.fromARGB(221, 30, 30, 30)
       ),
       home: MainScreenWidget(),
     );
--- lib/screens/main_screen_widget.dart
+++ lib/screens/main_screen_widget.dart
@@ -8,6 +8,9 @@ class MainScreenWidget extends StatefulWidget {
 }
 
 class _MainScreenWidgetState extends State<MainScreenWidget> {
+  bool isDarkTheme = false;
+  var backgroundColorDarkTheme = Color.fromARGB(221, 30, 30, 30);
+
   List<int> _marksList = [];
   List<int> marksForTeach = [2, 3, 4, 5];
   List<Color> borderColors = [
@@ -43,97 +46,134 @@ class _MainScreenWidgetState extends State<MainScreenWidget> {
     return sum / _marksList.length;
   }
 
+  void changeTheme() {
+    setState(() {
+      isDarkTheme = !isDarkTheme;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 35),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
+      backgroundColor: isDarkTheme ? backgroundColorDarkTheme : Colors.white,
+      body: Stack(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 35),
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    'Средний балл: ${_calculateAverage().toStringAsFixed(2)}',
-                    style: TextStyle(fontSize: 28),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Средний балл: ${_calculateAverage().toStringAsFixed(2)}',
+                        style: TextStyle(
+                          fontSize: 28,
+                          color: isDarkTheme ? Colors.white : Colors.black,
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              SizedBox(height: 50),
-              Text(
-                'Оценки: ${_marksList.join(', ')}',
-                style: TextStyle(fontSize: 24),
-              ),
-              SizedBox(height: 30),
+                  SizedBox(height: 50),
+                  Text(
+                    'Оценки: ${_marksList.join(', ')}',
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: isDarkTheme ? Colors.white : Colors.black,
+                    ),
+                  ),
+                  SizedBox(height: 30),
 
-              // Кнопки оценок
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: List.generate(marksForTeach.length, (index) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8),
-                      child: Container(
-                        height: 52,
-                        width: 75,
-                        child: ElevatedButton(
-                          onPressed: () => _addMark(marksForTeach[index]),
-                          style: ButtonStyle(
-                            foregroundColor:
-                                WidgetStatePropertyAll(Colors.black),
-                            side: WidgetStatePropertyAll(BorderSide(
-                              color: borderColors[index],
-                              width: 1.5,
-                            )),
+                  // Кнопки оценок
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(marksForTeach.length, (index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8),
+                        child: Container(
+                          height: 52,
+                          width: 75,
+                          child: ElevatedButton(
+                            onPressed: () => _addMark(marksForTeach[index]),
+                            style: ButtonStyle(
+                              foregroundColor: WidgetStatePropertyAll(isDarkTheme ? Colors.white: Colors.black),
+                              backgroundColor: WidgetStatePropertyAll(isDarkTheme ? backgroundColorDarkTheme: Colors.white),
+                              side: WidgetStatePropertyAll(BorderSide(
+                                color: borderColors[index],
+                                width: 1.5,
+                              )),
+                            ),
+                            child: Text(
+                              marksForTeach[index].toString(),
+                              style: TextStyle(fontSize: 16),
+                            ),
                           ),
+                        ),
+                      );
+                    }),
+                  ),
+                  SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 50,
+                        width: 170,
+                        child: ElevatedButton(
+                          onPressed: _deleteLastMark,
+                          style: exButtonStyle(),
                           child: Text(
-                            marksForTeach[index].toString(),
+                            '<—',
                             style: TextStyle(fontSize: 16),
                           ),
                         ),
                       ),
-                    );
-                  })),
-              SizedBox(height: 15),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                      height: 50,
-                      width: 170,
-                      child: ElevatedButton(
-                        onPressed: _deleteLastMark,
-                        style: exButtonStyle(),
-                        child: Text(
-                          '<—',
-                          style: TextStyle(fontSize: 16),
+                      SizedBox(width: 10),
+                      SizedBox(
+                        height: 50,
+                        width: 170,
+                        child: ElevatedButton(
+                          onPressed: _clearMarks,
+                          style: exButtonStyle(),
+                          child: Text(
+                            'Очистить',
+                            style: TextStyle(fontSize: 16),
+                          ),
                         ),
-                      )),
-                  SizedBox(width: 10),
-                  SizedBox(
-                      height: 50,
-                      width: 170,
-                      child: ElevatedButton(
-                        onPressed: _clearMarks,
-                        style: exButtonStyle(),
-                        child: Text(
-                          'Очистить',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                      )),
+                      ),
+                    ],
+                  ),
                 ],
               ),
-            ],
+            ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 42),
+            child: Align(
+              
+              alignment: Alignment.topRight, // Выравнивание по верхнему правому углу
+              child: IconButton(
+                onPressed: changeTheme,
+                icon: Icon(
+                  isDarkTheme ? Icons.light_mode_outlined : Icons.dark_mode_outlined,
+                  color: isDarkTheme ? Colors.yellow : Colors.black,
+                ),
+                iconSize: 32,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 
+
   ButtonStyle exButtonStyle() {
     return ButtonStyle(
-        foregroundColor: WidgetStatePropertyAll(Colors.black),
-        side: WidgetStatePropertyAll(BorderSide(color: Colors.black)));
+        foregroundColor: WidgetStatePropertyAll(isDarkTheme ? Colors.white : backgroundColorDarkTheme),
+        backgroundColor: WidgetStatePropertyAll(isDarkTheme ? Color.fromARGB(221, 30, 30, 30): Colors.white),
+        side: WidgetStatePropertyAll(BorderSide(color: isDarkTheme ? Colors.white : backgroundColorDarkTheme)));
   }
 }
